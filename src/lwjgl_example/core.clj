94d595eab2e6569a253013a7438b123b6d46699a
Crispin Wellington <retrogradeorbit@gmail.com>
2020-07-11 17:33:58 +0800

    start porting example code

--- src/lwjgl_example/core.clj
+++ src/lwjgl_example/core.clj
@@ -1,7 +1,22 @@
 (ns lwjgl-example.core
+  (:import [org.lwjgl Version]
+           [org.lwjgl.glfw GLFWErrorCallback])
   (:gen-class))
+
+(defn init! []
+  ;; Setup an error callback. The default implementation
+  ;; will print the error message in System.err.
+  (-> System/err
+      GLFWErrorCallback/createPrint
+      .set))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (str "Hello LWGL " (Version/getVersion) "!"))
+
+  (let [window (init!)]
+
+
+    )
+  )
